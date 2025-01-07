2013104946118be46c3b8c0e5e109cccdc4ba049
Saf98 <sbilimoria98@gmail.com>
2025-01-07 10:14:56 +0000

    return new array with values

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 //Write your function here
-function Hello() {
-    return "Hello World";
+function FizzBuzz() {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //Init Array 1...10
+    const Fizz = "Fizz";
+    const Buzz = "Buzz";
+    return arr.map((element, index) => {
+        if (element % 3 === 0 || element % 5 === 0) {
+            return arr[index] = Fizz+Buzz;
+        }
+    })
 }
 
-module.exports = Hello;
+console.log(FizzBuzz())
+
+module.exports = FizzBuzz;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,6 +2,6 @@
 
 const Hello = require('../src/index');
 
-test('Returns Hello World', () => {
-  expect(Hello()).toBe("Hello World");
+test('Returns New Arr', () => {
+  expect(Hello()).toBe([1, 2, "FizzBuzz", 4, "FizzBuzz", "FizzBuzz", 7, 8, "FizzBuzz", "FizzBuzz"]);
 });
